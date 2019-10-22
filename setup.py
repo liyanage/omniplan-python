d9f02cf260af35de5839b91d757d2028357f6bf1
Marc Liyanage <reg.gitcommit@entropy.ch>
2019-10-21 18:44:26 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="omniplan",
+    version="0.0.1",
+    author="Marc Liyanage",
+    author_email="reg.pythonmodule@entropy.ch",
+    description="Interact with OmniPlan",
+    long_description="Interact with OmniPlan",
+    long_description_content_type="text/markdown",
+    url="https://github.com/liyanage/omniplan-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: macOS",
+    ]
+)
